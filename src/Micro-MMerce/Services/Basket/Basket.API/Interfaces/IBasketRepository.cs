b6c8a9160ad8420c540a234eb2ddac7aba6ba24e
Akash Vekariya <akash.vekariya@cm.com>
2023-03-12 23:10:19 +0530

    Added `Basket` microservice

---
+++ src/Micro-MMerce/Services/Basket/Basket.API/Interfaces/IBasketRepository.cs
@@ -0,0 +1,11 @@
+using Basket.API.Models;
+
+namespace Basket.API.Interfaces;
+
+public interface IBasketRepository {
+
+   Task<ShoppingCart> GetBasket(string userName);
+   Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+   Task DeleteBasket(string userName);
+
+}
