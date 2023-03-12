78bc309295e2cd4ee6016575cdc8d378d7e3fa6d
Akash Vekariya <akash.vekariya@cm.com>
2023-03-12 23:10:19 +0530

    Added `Basket` microservice

---
+++ src/Micro-MMerce/Services/Basket/Basket.API/Models/ShoppingCartItem.cs
@@ -0,0 +1,21 @@
+namespace Basket.API.Models;
+
+public class ShoppingCartItem {
+
+   public string ProductId { get; set; }
+   public string ProductName { get; set; }
+   public decimal Price { get; set; }
+   public int Quantity { get; set; }
+   public string Color { get; set; }
+
+   public ShoppingCartItem() { }
+
+   public ShoppingCartItem(string productId, string productName, decimal price, int quantity, string color) {
+      ProductId = productId;
+      ProductName = productName;
+      Price = price;
+      Quantity = quantity;
+      Color = color;
+   }
+
+}
--- src/Micro-MMerce/Services/Basket/Basket.API/Program.cs
+++ src/Micro-MMerce/Services/Basket/Basket.API/Program.cs
@@ -1,8 +1,22 @@
+using Basket.API.Interfaces;
+using Basket.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager configuration = builder.Configuration;
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddStackExchangeRedisCache(
+   options => options.Configuration = configuration.GetValue<string>("CacheSettings:ConnectionString")
+);
+
+// ------------ dependency injection --------------------
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+// ------------------------------------------------------
 
 var app = builder.Build();
 
