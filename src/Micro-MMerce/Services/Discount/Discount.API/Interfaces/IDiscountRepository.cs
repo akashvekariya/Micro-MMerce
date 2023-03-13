275b6fa39d90e62edb71245637a2a803749da55c
Akash Vekariya <akashvekariya@yahoo.com>
2023-03-13 21:11:45 +0530

    Add `Discount` microservice

---
+++ src/Micro-MMerce/Services/Discount/Discount.API/Interfaces/IDiscountRepository.cs
@@ -0,0 +1,10 @@
+using Discount.API.Models;
+
+namespace Discount.API.Interfaces;
+
+public interface IDiscountRepository {
+   Task<Coupon> GetDiscount(string productName);
+   Task<bool> CreateDiscount(Coupon coupon);
+   Task<bool> UpdateDiscount(Coupon coupon);
+   Task<bool> DeleteDiscount(string productName);
+}
