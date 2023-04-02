d7335aedc6752c5d9da73a171ae084ffe3a62cd6
Akash Vekariya <akashvekariya@yahoo.com>
2023-04-02 12:14:50 +0530

    Add Ocelot API Gateway

--- src/Micro-MMerce/ApiGateways/OcelotApiGw/Program.cs
+++ src/Micro-MMerce/ApiGateways/OcelotApiGw/Program.cs
@@ -4,13 +4,13 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true);
+builder.Configuration.AddJsonFile($"Ocelot.{builder.Environment.EnvironmentName}.json", true, true);
 
 builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 builder.Logging.AddConsole();
 builder.Logging.AddDebug();
 
-builder.Services.AddOcelot().AddCacheManager(settings => settings.WithDictionaryHandle());
+builder.Services.AddOcelot(builder.Configuration).AddCacheManager(settings => settings.WithDictionaryHandle(true));
 
 var app = builder.Build();
 
